015dcc63894dd54d93c0ed39b624e093b788a75b
flov3rh <floperh@gmail.com>
2019-10-21 16:49:47 -0500

    work on the user interface

--- bin/main.rb
+++ bin/main.rb
@@ -5,12 +5,15 @@
 class Main
   attr_accessor :board
 
-  def initialize(board = [0, 0, 0, 0, 0, 0, 0, 0, 0])
+  def initialize(board=[" ", " ", " ", " ", " ", " ", " ", " ", " "])
     @board = board
+    @moves=0
   end
 
   def print_header
+    puts " "
     puts 'Welcome to tic tac toe'
+    puts " "
     puts '--------------------INSTRUCTIONS----------------------'
     puts 'To play this game, you need to get three in a row...'
     puts 'Your choice are define, they must bef from 1 to 9...'
@@ -23,20 +26,41 @@ class Main
     puts "Player one X is #{player_one} vs Player two O is #{player_two}"
     puts '**************************************************************'
     puts "This is the positions players can choose"
+    game_start(player_one,player_two)
+  end
+
+  def game_start(one,two)
+    while @moves<10
+      print_board
+      print_choice
+      puts "#{one} select the position to play: "
+      move_peg(gets.chomp,1)
+      puts "#{two} select the position to play: "
+      move_peg(gets.chomp,2)
+    end
+  end
+
+  def move_peg(move,player)
+    case player
+      when 1
+        @moves+=1
+        @board[(move.to_i-1)]="X";
+      when 2
+        @moves+=1
+        @board[(move.to_i-1)]="O";
+    end
     print_board
     print_choice
-    puts "#{player_one} select the position to play: "
-    puts "#{player_two} select the position to play: "
   end
 
   def print_board
-    puts "---------------"
-    puts " #{@board[0]} | #{@board[1]}  | #{@board[2]}  "
-    puts "---------------"
-    puts " #{@board[3]} | #{@board[4]}  | #{@board[5]}  "
-    puts "---------------"
-    puts " #{@board[6]} | #{@board[7]}  | #{@board[8]}  "
-    puts "---------------"
+    puts " "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]}"
+    puts " "
   end
 
   def print_choice
