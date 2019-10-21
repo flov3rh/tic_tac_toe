4514708f21e6cb67d38234a765a951d3660ad29f
J. Christian Romero <jcromerohdz@gmail.com>
2019-10-21 13:58:27 -0700

    Adding basic interface for the tic tac toe project

--- bin/main.rb
+++ bin/main.rb
@@ -2,4 +2,49 @@
 
 # frozen_string_literal: true
 
-puts 'Hello world!'
+class Main
+  attr_accessor :board
+
+  def initialize(board = [0, 0, 0, 0, 0, 0, 0, 0, 0])
+    @board = board
+  end
+
+  def print_header
+    puts 'Welcome to tic tac toe'
+    puts '--------------------INSTRUCTIONS----------------------'
+    puts 'To play this game, you need to get three in a row...'
+    puts 'Your choice are define, they must bef from 1 to 9...'
+    puts '------------------------------------------------------'
+    puts 'Please provide player one name: '
+    player_one = gets.chomp()
+    puts 'Please provide player two name: '
+    player_two = gets.chomp()
+    puts '**************************************************************'
+    puts "Player one X is #{player_one} vs Player two O is #{player_two}"
+    puts '**************************************************************'
+    puts "This is the positions players can choose"
+    print_board
+    print_choice
+    puts "#{player_one} select the position to play: "
+    puts "#{player_two} select the position to play: "
+  end
+
+  def print_board
+    puts "---------------"
+    puts " #{@board[0]} | #{@board[1]}  | #{@board[2]}  "
+    puts "---------------"
+    puts " #{@board[3]} | #{@board[4]}  | #{@board[5]}  "
+    puts "---------------"
+    puts " #{@board[6]} | #{@board[7]}  | #{@board[8]}  "
+    puts "---------------"
+  end
+
+  def print_choice
+    puts "1 | 2 | 3"
+    puts "4 | 5 | 6"
+    puts "7 | 8 | 9"
+  end
+end
+
+test = Main.new
+test.print_header
