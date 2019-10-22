54e148cd84bfbf1020791e9af6a5304ef6eb9d98
flov3rh <floperh@gmail.com>
2019-10-22 12:42:36 -0500

    created class to handle game logic

---
+++ lib/ticTacToe.rb
@@ -0,0 +1,13 @@
+class TicTacToe
+  def initialize(one,two,board=[" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    #Initialize our board and game
+    @board = board
+    @moves=0
+    @winner=
+    @player_one=one
+    @player_two=two
+  end
+
+
+
+end
